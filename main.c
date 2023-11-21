a74957a5bbd9d5656f87226512322829abfec5fc
LouisDecouzon <louis.decouzon.pro@gmail.com>
2023-11-21 14:44:23 +0100

    choix_joueur

--- main.c
+++ main.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
-
-
-
-enum couleurs {
-    Carreau=1,
-    Coeur=2,
-    Trefle=3,
-    Pique=4
+enum choix_joueur{
+    Hit=1,
+    Stand=2,
+    Double=3,
+    Surrend=4
 };
-
-
-
-
-
-int main() {
-    printf("Bob Bob!\n");
-    return 0;
-}
